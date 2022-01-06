2688232af7d36ac69cb1b1d48f149ecaf469f906
Henrique Oliveira <ohenrioliveira@gmail.com>
2022-01-06 11:19:42 -0300

    function studies

---
+++ funcoes/fun.js
@@ -0,0 +1,38 @@
+// Estudos sobre funções e suas particularidades
+/*function hello (){
+    console.log("Olá, Mundo!")
+}
+
+hello();*/
+
+
+
+/* function fun(parametro1, parametro2){
+    console.log(parametro1, parametro2)
+}
+
+fun("O valor é:", 2) */
+
+//FUNÇÃO DECLARATIVA:
+
+/* function fun(){
+    console.log("Essa é uma mensagem de uma função declarativa")
+}
+
+fun() */
+
+//EXPRESSÃO DE FUNÇÃO:
+
+/* var nome = function () {
+    console.log("Essa é uma mensagem de uma expressão de função")
+}
+
+nome() */
+
+//ARROW FUNCTION
+
+var nome = () => {
+    console.log("sou uma arrow function")
+}
+
+nome()
